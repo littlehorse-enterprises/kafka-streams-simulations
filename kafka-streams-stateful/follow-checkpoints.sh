914968b0b559c731ba3531734ae1190022207c47
Saúl Piña <saul@littlehorse.io>
2024-11-11 16:33:43 -0500

    refactor: add time to follow checkpoint

--- kafka-streams-stateful/follow-checkpoints.sh
+++ kafka-streams-stateful/follow-checkpoints.sh
@@ -2,4 +2,4 @@
 
 set -e
 cd $1
-watch -n 0.5 -t -c -d "echo 'Partitions with checkpoint file:' && find . -name '.checkpoint' -printf '%h\n' | sort -V"
+watch -n 0.1 -t -c -d "echo 'Partitions with checkpoint file:' && find . -name '.checkpoint' -exec ls -l --time-style=+'%T.%N' {} \; | cut -d ' ' -f '6-8' | sort -V -k1.22"
